e21451c47bed7747eed7503d98a001ed894bf175
markjohn519 <markjohnmonton@gmail.com>
2022-10-26 09:13:45 +0800

    modified index but following instruction now

--- index.js
+++ index.js
@@ -2,20 +2,13 @@ const { argv } = require('node:process')
 const fs = require('fs')
 
 const [,, inputFile, outputFile, flag] = argv
-if (!fs.existsSync(inputFile)) {
-  console.log('First Argument can\'t be emty')
-  process.exit(1)
-} else if (!fs.existsSync(outputFile)) {
-  const data = fs.readFileSync(inputFile, 'utf8')
-  const newArr = []
-  data.split('\n').forEach((line, index) => {
-    newArr.push(`${index + 1}: ${line}`)
-  })
-  const newString = newArr.join('\n').toString()
+if (inputFile === undefined) {
   process.stdin.on('data', (inputData) => {
-    fs.writeFileSync(inputData, newString, { encoding: 'utf8' })
-    console.log(`Succesfully duplicted ${inputFile} to ${outputFile}`)
-    process.exit(1)
+    const convertedInputData = inputData.toString().trim()
+    const newString = convertedInputData.split('\n')
+    newString.forEach((line, index) => {
+      console.log(`${index + 1}: ${line}`)
+    })
   })
 } else if (flag === '-y') {
   const dataArr = []
